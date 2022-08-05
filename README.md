759b060e02027ed5a02bcb70ad875f9c90570703
Diniz <barrosjunior26@outlook.com.br>
2022-08-05 14:00:54 -0300

    Foi inserido alguns links de imagens dentro do arquivo README e feito uma breve descrição do que se trata o programa desenvolvido.

---
+++ README.md
@@ -0,0 +1,9 @@
+# **Média entre duas notas e mostrando qual é a situação de aluno conforme a média final apresentada** 
+
+![Ver a imagem de origem](https://th.bing.com/th/id/R.8aa738b881f8f0a9a10190c3c228b08d?rik=RSHF831aVtU5RQ&riu=http%3a%2f%2farchivos.wikanda.es%2fcordobapedia%2fthumb%2fAprobado.png%2f120px-Aprobado.png&ehk=xjy5407xwBIBJhYp8ABHkA%2fhYyOSHV48lilN6klAxZ0%3d&risl=&pid=ImgRaw&r=0)
+
+![Ver a imagem de origem](https://www.tuinstitutoonline.com/cursos/calcavanzado1_v1406/material_alumno/img/03imagen.png)
+
+![Ver a imagem de origem](https://th.bing.com/th/id/R.8c9804dc238d66bc5c9ff7fa02bb60e4?rik=dZ3YUlynIQ%2bLDA&riu=http%3a%2f%2fimg4.wikia.nocookie.net%2f__cb20120618215139%2faonoexorcistsp%2fes%2fimages%2fa%2fa4%2fReprobado.png&ehk=NKPm6vgAn8Ne2AuGojPStVGAlD1oiWg9L1etG%2bScEY8%3d&risl=&pid=ImgRaw&r=0)
+
+Criar um programa que leia duas notas de um aluno e calcular a sua média, mostrando uma mensagem no final, de acordo com a média atingida.
---
+++ mediaDuasNotas/mediaDuasNotas/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Exercicio27
+{
+    class mediaDuasNotas
+    {
+        public static void Main(String[] args)
+        {
+            /*27) Crie um programa que leia duas notas de um aluno e calcule a sua média,
+            mostrando uma mensagem no final, de acordo com a média atingida:
+            -Média até 4.9: REPROVADO
+            - Média entre 5.0 e 6.9: RECUPERAÇÃO
+            - Média 7.0 ou superior: APROVADO*/
+
+            //Declaração de variáveis
+            int opcao = 0;
+
+            do
+            {   
+                //Solicitação das notas do aluno
+                Console.Write("Informe a primeira nota: ");
+                double nota1 = double.Parse(Console.ReadLine());
+                Console.Write("Informe a segunda nota: ");
+                double nota2 = double.Parse(Console.ReadLine());
+
+                //Processamento dos dados
+                double resultado = (nota1 + nota2) / 2;
+
+
+                //Seleção de resultado conforme a média final do aluno
+                if (resultado <= 4.9)
+                {
+                    Console.WriteLine($"O aluno está REPROVADO com média {resultado}.");
+                }else if (resultado >= 5 && resultado <= 6.9)
+                {
+                    Console.WriteLine($"O aluno está em RECUPERAÇÃO com média {resultado}.");
+                }
+                else
+                {
+                    Console.WriteLine($"O aluno está APROVADO com média {resultado}.");
+                }
+                //
+                //Solicitando se o usuário deseja realizar uma nova operação ou se deseja encerrar o sistema.
+                Console.WriteLine("Para NOVA OPERAÇÃO digite 1 ou 0 PARA SAIR do sistema");
+                opcao = int.Parse(Console.ReadLine());
+            } while (opcao != 0);
+            Console.WriteLine("Saindo do sistema...");
+        }
+    }
+}
